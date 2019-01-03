8fff730ed2987bf5e7de8377630dfc0d63b65b94
Jared Horne <thomhorne@gmail.com>
2019-01-03 09:47:27 -0600

    database seed file seeds mongoDB

--- mongoDB/seed.js
+++ mongoDB/seed.js
@@ -1,3 +1,32 @@
 const db = require('./index.js');
 const Song = require('./Song.js');
+const faker = require('faker');
 
+
+
+const createSongs = () => {
+  let songList = [];
+  for (let i = 0; i < 100; i++) {
+    let song = {
+      title: faker.random.words(),
+      artist: faker.name.findName(),
+      length: Math.floor(Math.random() * (480 - 180) + 180),
+      album: faker.random.words(),
+      order: Math.floor(Math.random() * (14 - 5) + 5),
+      imageURL: faker.image.imageUrl()
+    };
+    songList.push(song);
+  }
+  return songList;
+}
+
+const seedDB = () => {
+  return new Promise((resolve, reject) => {
+      resolve(createSongs());
+  }).then((songs) => {
+    Song.create(songs)
+    .then(() => db.connection.close());
+  })
+}
+
+seedDB();
