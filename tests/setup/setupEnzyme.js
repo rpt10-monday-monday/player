89f52b45a4bf6792cf58e00e19d632201d24c448
Jared Horne <thomhorne@gmail.com>
2019-01-03 15:15:40 -0600

    enzyme and jest working, no tests passing

--- tests/setup/setupEnzyme.js
+++ tests/setup/setupEnzyme.js
@@ -1,3 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+// const Enzyme = require('enzyme');
+// const Adapter = require('enzyme-adapter-react-16');
+
 Enzyme.configure({ adapter: new Adapter() });
+
