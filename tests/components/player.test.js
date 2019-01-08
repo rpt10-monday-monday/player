ac8ed346e6584d2c2093eb7a1b736f3fbbee798c
Jared Horne <thomhorne@gmail.com>
2019-01-08 11:12:04 -0600

    first test passing properly rendering Player component

--- tests/components/player.test.js
+++ tests/components/player.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import renderer from 'react-test-renderer';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 import { shallow, mount, render } from 'enzyme';
 import toJson from 'enzyme-to-json';
+// import '../../client/components/controls.css';
 
 import Player from '../../client/components/player.jsx';
 
+// describe('<Player />', () => {
+//   describe('render()', () => {
+//   test('renders the component', () => {
+//     const wrapper = shallow(<Player />);
+//     expect(wrapper).toMatchSnapshot();
+//     })
+//   })
+// });
+// describe('Player', () => {
+//   test('it renders', () => {
+//     const wrapper = shallow(<Player />)
+//     expect(wrapper).toMatchSnapshot();
+//     // expect(true).toBeTruthy();
+//   })
+// })
+
+test('Player renders', () => {
+  const wrapper = shallow(<Player />)
+  expect(wrapper).toMatchSnapshot();
+})
