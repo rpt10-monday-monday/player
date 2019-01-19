a8cfd5664f5b25b07bb3dbb62bfcdd29e61859ad
Jared Horne <thomhorne@gmail.com>
2019-01-19 10:08:50 -0600

    changing websocket address

--- client/components/player.jsx
+++ client/components/player.jsx
@@ -38,7 +38,7 @@ const Artist = styled.div`
   display: inline-block;
 `
 
-const socket = io('http://audioplayer-dev.us-east-2.elasticbeanstalk.com/', {transports: ['websocket']});
+const socket = io('ws://audioplayer-dev.us-east-2.elasticbeanstalk.com/', {transports: ['websocket']});
 
 
 const registerHandler = (onMsgReceived) => {
